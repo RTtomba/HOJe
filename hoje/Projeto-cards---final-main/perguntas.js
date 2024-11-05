c25520a669deab1b779fdb0e7ab766f757b01865
ruanzin <112881520+RTtomba@users.noreply.github.com>
2024-11-05 07:52:56 -0300

    Add files via upload

---
+++ hoje/Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,64 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço na memória que armazena um valor.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa “debugging” em programação?',
+    'Debugging é o processo de encontrar e corrigir erros no código'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop em programação?',
+    'Um loop é uma estrutura que repete um conjunto de instruções várias vezes.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre uma variável e uma constante?',
+    'A variável pode ter seu valor alterado, enquanto a constante mantém o valor fixo.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função em programação?',
+    'Uma função é um bloco de código que realiza uma tarefa específica e pode retornar um valor.'
+)
+criaCartao(
+    'Programação',
+    'O que significa “compilar” um programa?',
+    'Compilar é o processo de transformar o código-fonte em código de máquina executável.'
+)
+criaCartao(
+    'Programação',
+    'O que é um array?',
+    'Um array é uma estrutura que armazena múltiplos valores em uma única variável.'
+)
+criaCartao(
+    'Programação',
+   'O que é um “if” em programação?',
+   'O “if” é uma estrutura condicional que executa um bloco de código se uma condição for verdadeira.'
+)
+criaCartao(
+    'Programação',
+    'O que é um loop “for”?',
+    'O “for” é um tipo de loop usado para repetir um bloco de código um número específico de vezes.'
+)
+criaCartao(
+    'Programação',
+    'O que significa “sintaxe” em programação?',
+    'Sintaxe é a forma correta de escrever o código, com regras que a linguagem de programação exige.'
+)
+criaCartao(
+    'Programação',
+    'O que é um “algoritmo”?',
+    'Um algoritmo é uma sequência de passos para resolver um problema.'
+)
+criaCartao(
+    'Programação',
+    '	O que é uma linguagem de programação?',
+    'Uma linguagem de programação é um conjunto de regras e sintaxe para escrever programas de computador.'
+)
